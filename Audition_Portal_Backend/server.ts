89769766df3c563d376bf5408781d1b6cc617d43
Shreya Birmecha <shreyabirmecha8@gmail.com>
2026-01-13 23:12:29 +0530

    changed ts output for render

--- Audition_Portal_Backend/server.ts
+++ Audition_Portal_Backend/server.ts
@@ -183,7 +183,10 @@ app.use("/api/round2", round2Router);
 app.use("/api/admin/r1", adminRoundOneRouter);
 app.use("/api/admin/r2", adminRoundTwoRouter);
 
-app.listen(8080, () => {
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
   connectToDatabase();
-  console.log("Server is running on port 8080");
+  console.log("Server is running on port", PORT);
 });
+
